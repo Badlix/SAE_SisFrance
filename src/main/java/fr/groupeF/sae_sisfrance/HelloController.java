3dfba8c1c40fd91849b8f19b7f3015eecbf2737a
Alex <alex.possenti0@gmail.com>
2023-06-06 17:09:56 +0200

    changement de page avec boutons

--- src/main/java/fr/groupeF/sae_sisfrance/HelloController.java
+++ src/main/java/fr/groupeF/sae_sisfrance/HelloController.java
@@ -5,11 +5,16 @@ import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -38,6 +43,18 @@ public class HelloController extends BorderPane implements Initializable {
         regionColumn.setCellValueFactory(new PropertyValueFactory<>("region"));
         intensiteColumn.setCellValueFactory(new PropertyValueFactory<>("intensite"));
 //        updateTable();
+    }
+    @FXML
+    private void changingPageButton(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("filePickerPage.fxml"));
+            Scene scene = new Scene(loader.load(), 1000, 500);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     @FXML
     public void buttonFilterRegion(ActionEvent actionEvent) {
