4dd589b62eafd69c26bad43e13ec072cf34a5e8c
Badlix <mathieu.leroux@etu.univ-amu.fr>
2023-06-06 09:35:00 +0200

    changement minime sur les fichiers fxml

--- src/main/java/fr/groupeF/sae_sisfrance/HelloController.java
+++ src/main/java/fr/groupeF/sae_sisfrance/HelloController.java
@@ -4,12 +4,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("test changement");
-    }
+
 }
-//jhyvfbdsqoninccc
