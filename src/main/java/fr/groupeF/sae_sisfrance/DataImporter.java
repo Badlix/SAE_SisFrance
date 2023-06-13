0715b115bc6cfed64beccf5b415e3abb3fd25736
Alexandre possenti <alex.possenti0@gmail.com>
2023-06-13 11:15:47 +0200

    retour

--- src/main/java/fr/groupeF/sae_sisfrance/DataImporter.java
+++ src/main/java/fr/groupeF/sae_sisfrance/DataImporter.java
@@ -4,8 +4,34 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class DataImporter {
+
+    public static List<String> parseString(String inputString) {
+        List<String> result = new ArrayList<>();
+        StringBuilder currentItem = new StringBuilder();
+        boolean insideQuotes = false;
+
+        for (char c : inputString.toCharArray()) {
+            if (c == ',' && !insideQuotes) {
+                if (currentItem.length() == 0) {
+                    result.add("");
+                } else {
+                    if (currentItem.charAt(currentItem.length()-1) == '/')
+                        currentItem.deleteCharAt(currentItem.length()-1);
+                    result.add(currentItem.toString().trim());
+                    currentItem = new StringBuilder();
+                }
+            } else if (c == '"') {
+                insideQuotes = !insideQuotes;
+            } else {
+                currentItem.append(c);
+            }
+        }
+        result.add(currentItem.toString().trim());
+        return result;
+    }
     public static ArrayList<Earthquake> readCSV(File csvFile) {
         String line;
         ArrayList<Earthquake> earthquakes = new ArrayList<Earthquake>();
@@ -13,9 +39,7 @@ public class DataImporter {
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
             br.readLine();
             while ((line = br.readLine()) != null) {
-                System.out.println(line);
-                String[] data = line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
-                System.out.println(data.length);
+                List<String> data = parseString(line);
                 // Process the data
                 Earthquake earthquake = new Earthquake(data);
                 earthquakes.add(earthquake);
